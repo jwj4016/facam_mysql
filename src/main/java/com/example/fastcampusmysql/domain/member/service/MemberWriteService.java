6c9148868e04b04c7e0fb693ca91b731f9288c2b
J <jwj4016@gmail.com>
2023-02-02 21:10:36 +0900

    회원 정보 등록 기능 추가

--- src/main/java/com/example/fastcampusmysql/domain/member/service/MemberWriteService.java
+++ src/main/java/com/example/fastcampusmysql/domain/member/service/MemberWriteService.java
@@ -2,8 +2,16 @@ package com.example.fastcampusmysql.domain.member.service;
 
 import com.example.fastcampusmysql.domain.member.dto.RegisterMemberCommand;
 import com.example.fastcampusmysql.domain.member.entity.Member;
+import com.example.fastcampusmysql.domain.member.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Required;
+import org.springframework.stereotype.Service;
 
+@RequiredArgsConstructor
+@Service
 public class MemberWriteService {
+    final private MemberRepository memberRepository;
+
     public void create(RegisterMemberCommand command) {
 
         /**
@@ -17,5 +25,7 @@ public class MemberWriteService {
                 .email(command.email())
                 .birthday(command.birthday())
                 .build();
+
+        memberRepository.save(member);
     }
 }
