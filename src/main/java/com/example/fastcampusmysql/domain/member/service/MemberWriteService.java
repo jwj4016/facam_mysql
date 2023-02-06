00edd674cd4a12ebba13ea1facaa99f455818bcc
J <jwj4016@gmail.com>
2023-02-06 15:51:04 +0900

    회원 닉네임 변경 기능 및 테스트 코드 추가

--- src/main/java/com/example/fastcampusmysql/domain/member/service/MemberWriteService.java
+++ src/main/java/com/example/fastcampusmysql/domain/member/service/MemberWriteService.java
@@ -13,7 +13,7 @@ public class MemberWriteService {
 
     public Member register(RegisterMemberCommand command) {
 
-        /**
+        /*
          * 목표 - 회원정보(이메일, 닉네임, 생년월일)를 등록한다. 닉네임은 10자를 넘길 수 없다.
          * 파라미터 - memberRegisterCommand
          * val member = Member.of(memberRegisterCommand)
@@ -26,5 +26,15 @@ public class MemberWriteService {
                 .build();
 
         return memberRepository.save(member);
+    }
+
+    public void changeNickname(Long memberId, String nickname) {
+        /*
+            1.회원의 이름을 변경
+            2. 변경 내역을 저장한다.
+         */
+        var member = memberRepository.findById(memberId).orElseThrow();
+        member.changeNickname(nickname);
+
     }
 }
